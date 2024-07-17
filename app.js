ca988cc3824d3e493f80c5e0f0dd787410657b9e
Lmc95 <95carranzalm@gmail.com>
2024-07-17 09:21:15 -0300

    test6

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const h1_txt = document.getElementById('text_h1');
     fetch('/.netlify/functions/env')
-        .then(res => res)
+        .then(res => JSON.stringify(res))
         .then(data => {
             console.log(data)
             h1_txt.textContent = data;
