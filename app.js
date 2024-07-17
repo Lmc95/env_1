e141f40e641d34d504402f670dc4865c69435370
Lmc95 <95carranzalm@gmail.com>
2024-07-17 09:15:20 -0300

    test2

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-// // app.js
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Obtener el valor de la variable de entorno
-//     const myEnvVar = process.env.MY_ENV_VAR;
-    
-//     // Mostrar el valor en el h1 del HTML
-//     const h1Element = document.querySelector('h1');
-//     h1Element.textContent = myEnvVar;
-// });
-// app.js
-// app.js
-document.addEventListener('DOMContentLoaded', function() {
-    // Hacer una solicitud a la función de Netlify
-    fetch('/api/env')
-        .then(response => response.json())
+document.addEventListener('DOMContentLoaded', () => {
+    const h1_txt = document.getElementById('text_h1');
+    fetch('/.netlify/functions/env')
+        .then(res => res.json())
         .then(data => {
-            const myEnvVar = data.message;  // Obtener el valor de la variable de entorno desde la respuesta
-            // Mostrar el valor en el h1 del HTML
-            const h1Element = document.querySelector('h1');
-            h1Element.textContent = myEnvVar;
+            console.log(data)
+            h1_txt.textContent = data.myEnvVar;
         })
-        .catch(error => {
-            console.error('Error al obtener la variable de entorno:', error);
-        });
-});
+
+})
