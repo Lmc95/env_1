ee0ec05ec3d55fadedf456ecd34e330c79c8f805
Lmc95 <95carranzalm@gmail.com>
2024-07-17 09:19:39 -0300

    test5

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const h1_txt = document.getElementById('text_h1');
     fetch('/.netlify/functions/env')
-        .then(res => res.json())
+        .then(res => res)
         .then(data => {
             console.log(data)
             h1_txt.textContent = data;
